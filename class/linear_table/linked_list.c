a5a4995955e825bdabb69bdf26c9709d12f8c48e
yichuanshuzi <chenboshuo.hi@163.com>
2019-09-19 20:09:15 +0800

    :sparkles: add the function of creat list. 增加创建字符型链表的函数

--- class/linear_table/linked_list.c
+++ class/linear_table/linked_list.c
@@ -10,7 +10,7 @@ typedef struct node {
 
 /**
  * 初始化链表
- * @return 头结点
+ * @return 指向头结点指针
  * 算法思路
  * 1.让head指向一个新的重新分配的节点, 指针域为空
  * 2.返回head;
@@ -19,4 +19,30 @@ linked_list *init() {
   linked_list *head = (linked_list *)malloc(sizeof(linked_list));
   head->next = NULL;
   return head;
+}
+
+/**
+ * 创建列表(仅限于char),输入"$"停止
+ * @param  head 头指针
+ * @return      尾部指针
+ */
+linked_list *creat_char_list(linked_list *head) {
+  linked_list *tail, *p;
+  type x;
+  x = getchar();
+  while (x != '$') {
+    p = (linked_list *)malloc(sizeof(linked_list));
+    p->data = x;
+    p->next = NULL;
+    tail->next = p;
+    tail = p;
+    x = getchar();
+  }
+  return tail;
+}
+
+int main(int argc, char const *argv[]) {
+  linked_list *head = init();
+  creat_char_list(head);
+  return 0;
 }
